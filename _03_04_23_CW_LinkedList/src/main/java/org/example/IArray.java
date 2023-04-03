6d1b726cd1c01d59e7aefad84874f07af1f27766
DavidHaykov <mr.haykov@icloud.com>
2023-04-03 18:33:26 +0300

    Interface add

---
+++ _03_04_23_CW_LinkedList/src/main/java/org/example/IArray.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Comparator;
+import java.util.function.Predicate;
+
+public interface IArray<T> extends Iterable<T>
+{
+    boolean add(T obj);
+    boolean add(int index, T obj);
+    T get(int index);
+    int size();
+    int indexOf(T obj);
+    int lastIndexOf(T obj);
+    T remove(int index);
+    boolean remove(T obj);
+    boolean contains(T obj);
+    Object[] toArray();
+
+    boolean set(T obj, int index);
+    void addAll(IArray<T> other);
+    void addAll(int index, IArray<T> other);
+    boolean removeAll(T obj);
+
+    void sort(Comparator<T> comp);
+
+    boolean removeIf(Predicate<T> pred);
+    int indexOf(Predicate<T> pred);
+    int lastIndexOf(Predicate<T> pred);
+}
---
+++ _03_04_23_CW_LinkedList/src/main/java/org/example/MyLinkedList.java
@@ -0,0 +1,102 @@
+package org.example;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.function.Predicate;
+
+public class MyLinkedList <T> implements IArray<T> {
+    @Override
+    public boolean add(T obj) {
+        return false;
+    }
+
+    @Override
+    public boolean add(int index, T obj) {
+        return false;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int indexOf(T obj) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(T obj) {
+        return 0;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(T obj) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(T obj) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean set(T obj, int index) {
+        return false;
+    }
+
+    @Override
+    public void addAll(IArray<T> other) {
+
+    }
+
+    @Override
+    public void addAll(int index, IArray<T> other) {
+
+    }
+
+    @Override
+    public boolean removeAll(T obj) {
+        return false;
+    }
+
+    @Override
+    public void sort(Comparator<T> comp) {
+
+    }
+
+    @Override
+    public boolean removeIf(Predicate<T> pred) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(Predicate<T> pred) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Predicate<T> pred) {
+        return 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
