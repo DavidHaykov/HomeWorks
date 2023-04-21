955136e38f88de5e5e528b0ff23eaaae0148bb58
DavidHaykov <mr.haykov@icloud.com>
2023-04-21 11:49:20 +0300

    First commit

---
+++ _20_04_23_HW/src/main/java/telran/employees/Main.java
@@ -0,0 +1,13 @@
+package telran.employees;
+
+import telran.employees.dto.Programmer;
+import telran.employees.service.ProgrammersMaps;
+
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        //ProgrammersMaps map = new ProgrammersMaps();
+
+    }
+}
