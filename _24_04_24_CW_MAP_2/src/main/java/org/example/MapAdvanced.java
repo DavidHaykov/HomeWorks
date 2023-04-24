a908fb8ff031e87cdca21b338b635e2d2fd070ab
DavidHaykov <mr.haykov@icloud.com>
2023-04-24 20:54:15 +0300

    First commit

--- _24_04_24_CW_MAP_2/src/main/java/org/example/MapAdvanced.java
+++ _24_04_24_CW_MAP_2/src/main/java/org/example/MapAdvanced.java
@@ -1,8 +1,8 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class MapAdvanced {
     public static void main(String[] args) {
@@ -15,7 +15,76 @@ public class MapAdvanced {
             }
         });
         System.out.println(month);
+
+        month.compute("Dec", new BiFunction<String, Integer, Integer>() {
+            @Override
+            public Integer apply(String s, Integer integer) {
+                return integer==null? 100 : integer + 1;
+            }
+        });
+        System.out.println(month);
+
+        month.computeIfAbsent("Jun", new Function<String, Integer>() {
+            @Override
+            public Integer apply(String s) {
+                int res = Integer.MAX_VALUE;
+                res -= 150;
+                return res;
+            }
+        });
+        System.out.println(month);
+
+        month.computeIfPresent("Jan", new BiFunction<String, Integer, Integer>() {
+            @Override
+            public Integer apply(String s, Integer integer) {
+                return integer*10;
+            }
+        });
+        System.out.println(month);
+
+        System.out.println(reverseMap(month));
+
+        month.merge("Jan", 1, new BiFunction<Integer, Integer, Integer>() {
+            @Override
+            public Integer apply(Integer integer, Integer integer2) {
+                return integer+integer2;
+            }
+        });
+        System.out.println(month);
+
+        month.merge("Oct", 1, new BiFunction<Integer, Integer, Integer>() {
+            @Override
+            public Integer apply(Integer integer, Integer integer2) {
+                return integer+integer2;
+            }
+        });
+        System.out.println(month);
+        month.merge("Oct", 1, new BiFunction<Integer, Integer, Integer>() {
+            @Override
+            public Integer apply(Integer integer, Integer integer2) {
+                return null;
+            }
+        });
+        System.out.println(month);
     }
+    private static Map<Integer, List<String>> reverseMap(Map<String, Integer> map){
+        Map<Integer, List<String>> res= new HashMap<>();
+        for(String str : map.keySet()){
+            Integer value = map.get(str);
+            res.compute(value, new BiFunction<Integer, List<String>, List<String>>() {
+                @Override
+                public List<String> apply(Integer integer, List<String> strings) {
+                    if(strings == null ){
+                        strings = new ArrayList<>();
+                    }
+                    strings.add(str);
+                    return strings;
+                }
+            });
+        }
+        return res;
+    }
+
     private static void fillMap(Map<String, Integer> month){
         month.put("Jan", 1);
         month.put("Feb", 2);
